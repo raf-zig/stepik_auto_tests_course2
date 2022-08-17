bd21616aa8c0c5f517f4441e4306e8fe467dcdc1
unknown <ziravin@yandex.ru>
2022-08-17 21:57:59 +0500

    autotests_for_different__languages_interfaces

---
+++ test_items.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_basket_button(browser):
+    browser.get(link)
+    time.sleep(30)
+    element = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+    assert element, "not"
