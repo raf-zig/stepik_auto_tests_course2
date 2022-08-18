9e8cf2b331a768dbf97e0790bf0ec158f7ffef23
unknown <ziravin@yandex.ru>
2022-08-18 12:12:36 +0500

    changing the assert

--- conftest.py
+++ conftest.py
@@ -10,7 +10,6 @@ def pytest_addoption(parser):
     
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
--- test_items.py
+++ test_items.py
@@ -7,4 +7,4 @@ def test_basket_button(browser):
     browser.get(link)
     time.sleep(30)
     element = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
-    assert element, "not"
+    assert element is not None, "not"
